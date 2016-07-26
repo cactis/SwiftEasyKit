ec97e3b2985d653772c0fa0f47e0462215651357
cactis <cactis.lin@gmail.com>
2016-07-26 13:32:27 +0800

    Initial Commit

---
+++ SwiftEasyKit/SwiftEasyKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEasyKit.h
+//  SwiftEasyKit
+//
+//  Created by ctslin on 7/26/16.
+//  Copyright © 2016 airfont. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEasyKit.
+FOUNDATION_EXPORT double SwiftEasyKitVersionNumber;
+
+//! Project version string for SwiftEasyKit.
+FOUNDATION_EXPORT const unsigned char SwiftEasyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEasyKit/PublicHeader.h>
+
+
