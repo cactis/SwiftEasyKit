40f9e9ed1f597e1005e187c9f9838c049fd6bc9b
cactis <cactis.lin@gmail.com>
2016-07-28 14:57:52 +0800

    dummy

---
+++ Source/Extensions/Development.swift
@@ -0,0 +1,18 @@
+//
+//  Development.swift
+//  SwiftEasyKit
+//
+//  Created by ctslin on 7/28/16.
+//  Copyright © 2016 airfont. All rights reserved.
+//
+
+import Foundation
+
+public struct Development {
+  //  static var simulator = true
+  static var simulator = false
+  static var mode = "UI Design"
+  //  static var mode = "API Implement"
+}
+
+
