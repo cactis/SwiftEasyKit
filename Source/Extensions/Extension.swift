06269c52f38513476b7f802792fd21213ebf1f6e
cactis <cactis.lin@gmail.com>
2017-04-24 15:58:36 +0800

    dummy

--- Source/Extensions/Extension.swift
+++ Source/Extensions/Extension.swift
@@ -16,6 +16,26 @@ import Fakery
 
 public let faker = Faker()
 
+public extension UIWindow {
+  public var visibleViewController: UIViewController? {
+    return UIWindow.getVisibleViewControllerFrom(self.rootViewController)
+  }
+  
+  public static func getVisibleViewControllerFrom(_ vc: UIViewController?) -> UIViewController? {
+    if let nc = vc as? UINavigationController {
+      return UIWindow.getVisibleViewControllerFrom(nc.visibleViewController)
+    } else if let tc = vc as? UITabBarController {
+      return UIWindow.getVisibleViewControllerFrom(tc.selectedViewController)
+    } else {
+      if let pvc = vc?.presentedViewController {
+        return UIWindow.getVisibleViewControllerFrom(pvc)
+      } else {
+        return vc
+      }
+    }
+  }
+}
+
 extension NSMutableDictionary {
   public func jsonString(onSuccess: (string: NSString) -> ()) {
     do {
