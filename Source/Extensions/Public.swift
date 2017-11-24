daab91f9e0519ea82a4df5b45ce804fcd0467b61
cactis <cactis.lin@gmail.com>
2017-11-24 12:30:24 +0800

    dummy

--- Source/Extensions/Public.swift
+++ Source/Extensions/Public.swift
@@ -42,7 +42,7 @@ public func prompt(_ msgs: [String], runWith: (_ msg: String) -> String = { msg 
 }
 
 public func prompt(_ msg: String?, style: PromptType = PromptType(), onTapped: @escaping () -> () = {}) {
-  if !Development.prompt { return }
+//  if !Development.prompt { return }
   let message = msg ?? "(異常錯誤：未被追蹤到的錯誤。)"
   UIApplication.shared.isStatusBarHidden = true
   let notification = UIButton()
