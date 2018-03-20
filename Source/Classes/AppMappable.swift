549f7fc7141bf5cfa0985d7a51ba3613c3c2d074
cactis <cactis.lin@gmail.com>
2018-03-20 20:50:40 +0800

    dummy

--- Source/Classes/AppMappable.swift
+++ Source/Classes/AppMappable.swift
@@ -24,7 +24,7 @@ open class AppMappable: Mappable {
   
   open func mapping(map: Map) {    
     id <- map["id"]
-    state <- map["aasm_state"]
+    state <- map["state"]
 //    state <- map["state"]
 //    status <- map["status"]
     createdAt <- (map["created_at"], DateTransform(timeFormat:  AppMappable.TIMEFORMAT))
